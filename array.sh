df5410401862d5edd14b563c1edc09d683070e64
kevinwto <kevinwto01@gmail.com>
2020-01-14 10:20:26 -0800

    add updated assay.sh and c-echo.cpp files

--- array.sh
+++ array.sh
@@ -1,12 +1,18 @@
 
 #!/bin/sh
 
-INPUTS = ("first input" "second input" "third input")
+INPUTS=("first input" "second input" "third input")
 
-echo "Executing ./c-echo ${INPUTS[2]}"
-./c-echo ${INPUTS[2]}
-echo "Executing ./c-echo ${INPUTS[1]}"
-./c-echo ${INPUTS[1]}
-echo "Executing ./c-echo ${INPUTS[0]}"
-./c-echo ${INPUTS[0]}
-
+for input in "${INPUTS[@]}"
+do
+    echo "./c-echo ${input}"
+    output=$(./c-echo ${input})
+    if [ "${output}" = "${input}" ]
+    then
+        echo "Test passed"
+    else
+        echo "Test failed"
+        echo "Expected: \"${input}\""
+        echo "Received: \"${output}\""
+    fi
+done
